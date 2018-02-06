2983d3de43d45f9ca1abee132a06445f67ac3593
GURUIQI <gurq@asiainfo.com>
2018-02-06 15:35:05 +0800

    Version -1.4 实现按分类、作者的搜索功能

--- js/myjs.js
+++ js/myjs.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
 					authorPath =  item.path;
 					}
 			 }) ;
-			return  authorPath;s
+			return  authorPath;
 	}
 	//确定博文图片
 	function getBlogImage(array){
@@ -228,16 +228,37 @@ $(document).ready(function () {
 
     //搜索  searchdata
   $(".search-text").bind("input propertychange",function () {
-  	var userInput = $(this).val();
-  	var searchresult = "";
-		$.each(searchdata,function (i, sdata) {
-			if (sdata.test(userInput)){
-                searchresult += ""
-			}
-        })
-
+	var userInput = $(this).val();
+	var searchresult = "";
+	var subResult = "";
+	if ( userInput != ""){
+        $.each(searchdata,function (i, sdata) {
+            if (sdata.toLowerCase().indexOf(userInput.toLowerCase())>=0){
+            	var existFlag = 0;
+            	var authorBlogPath = ""
+            	$.each(authors,function (i, author) {
+					if (author.name == sdata){
+                        existFlag = 1;
+                        authorBlogPath = author.path;
+					}
+                });
+				if (existFlag == 1){
+                    subResult = '<li class="searchresultitem"><a target="_blank" href="'+siteurl+authorBlogPath+'">'+sdata+'</li> </a> ';
+				}else{
+                    subResult = '<li class="searchresultitem"><a target="_blank" href="'+siteurl+'/categories/'+sdata+'">'+sdata+'</li></a> ';
+				}
+                searchresult += subResult;
+                subResult = "";
+            }
+        });
+	}
+	$(".searchresult").html(searchresult);
   })
 
+    $(document).click(function(){
+        $(".searchresult").html("");
+        $(".search-text").val("");
+    })
 	//延时加载
   $(window).scroll(function () {
   	var  windowHeight = window.innerHeight;
